d7fdc34c8f46610cc60843c1e875edd38ea863ab
Strocs <strocsdev@gmail.com>
2024-12-10 22:32:06 -0300

    add array notes (none)

---
+++ arrays/Main.java
@@ -0,0 +1,14 @@
+package arrays;
+
+public class Main {
+  public static void main(String[] args) {
+    // for each
+    int[] values = { 1, 2, 3, 4, 5 };
+    int sum = 0;
+    for (int value : values) {
+      // cant modify the value;
+      sum += value;
+    }
+    System.out.println(sum);
+  }
+}
