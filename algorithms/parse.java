8cad82c79f861509b0d064cf6508197c69122dd8
Strocs <strocsdev@gmail.com>
2024-12-10 22:31:58 -0300

    add insertion sort on algorithms

---
+++ algorithms/parse.java
@@ -0,0 +1,11 @@
+package algorithms;
+
+public class parse {
+  public static int[] args(String[] args) {
+    int[] result = new int[args.length];
+    for (int i = 0; i < args.length; i++) {
+      result[i] = Integer.parseInt(args[i]);
+    }
+    return result;
+  }
+}
